ebd0754a1bc37c88c2e727cb90285bdd56310a2c
Marin Nikolli <sibalonat@gmail.com>
2023-11-20 22:36:44 +0100

    urls

--- marinit/urls.py
+++ marinit/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include('hello.urls')),
+    path('newyear/', include('checknewyear.urls')),
 ]
