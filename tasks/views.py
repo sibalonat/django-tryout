658585a5c759753e250d00238bb004d842dd2c6d
Marin Nikolli <sibalonat@gmail.com>
2023-11-20 23:28:28 +0100

    display variable array

--- tasks/views.py
+++ tasks/views.py
@@ -5,4 +5,6 @@ from django.shortcuts import render
 tasks = ['foo', 'bar', 'baz']
 
 def index(request) :
-    return render(request, 'tasks/index.html')
+    return render(request, 'tasks/index.html', {
+        'tasks': tasks
+    })
