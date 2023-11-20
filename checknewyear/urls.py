1947905385aca09225188510b2a01ac22863ee5b
Marin Nikolli <sibalonat@gmail.com>
2023-11-20 22:39:39 +0100

    urls for new years check

---
+++ checknewyear/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # path('marin', views.marin, name='marin'),
+    # path('<str:name>', views.fry, name='fry'),
+]
