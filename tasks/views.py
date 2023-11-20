b7b8f3f9fc1baad55eb2e17d95bb83c8ecca2ffa
Marin Nikolli <sibalonat@gmail.com>
2023-11-20 23:27:19 +0100

    create some sample tasks

--- tasks/views.py
+++ tasks/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
+tasks = ['foo', 'bar', 'baz']
+
 def index(request) :
-    return render(request, 'hello/index.html')
+    return render(request, 'tasks/index.html')
