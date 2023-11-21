674e2f8477b94862f40288f288233d3a4eaaf865
Marin Nikolli <sibalonat@gmail.com>
2023-11-21 13:57:02 +0100

    .

--- tasks/views.py
+++ tasks/views.py
@@ -22,7 +22,8 @@ def add(request) :
         form = NewTaskForm(request.POST)
         if form.is_valid() : 
             task = form.cleaned_data['task']
-            tasks.append(task)
+            request.session['tasks'] += [task]
+            # .append(task)
             return HttpResponseRedirect(reverse('tasks:index'))
         else :
             return render(request, 'tasks/add.html', {
