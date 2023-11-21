ee0f580035e1e7480aef2c9e8da3d4907ded6ddc
Marin Nikolli <sibalonat@gmail.com>
2023-11-21 12:57:20 +0100

    reverse

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django import forms 
+from django.http import HttpResponseRedirect 
+from django.urls import reverse 
 # Create your views here.
 
 tasks = ['foo', 'bar', 'baz']
