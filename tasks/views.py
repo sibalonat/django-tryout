e48853b0b9525545463ff5a5f42f87310623450b
Marin Nikolli <sibalonat@gmail.com>
2023-11-21 12:30:17 +0100

    .

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from django import forms 
 # Create your views here.
 
 tasks = ['foo', 'bar', 'baz']
+
+class NewTaskForm()
 
 def index(request) :
     return render(request, 'tasks/index.html', {
